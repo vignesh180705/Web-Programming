024a3d164dafbe12fa4219c5819a86d5c3fc3b12
Vignesh N <vignesh.n2023@vitstudent.ac.in>
2025-02-05 22:54:37 +0530

    Delete Lab8/Web Ex-8/Lab6 directory

--- Lab8/Web Ex-8/Lab6/entry.js
+++
@@ -1,6 +0,0 @@
-function calc(){
-    a=parseInt(document.getElementById("adult").value);
-    c=parseInt(document.getElementById("children").value);
-    t=a*10+c*5
-    document.getElementById("total").value=t;
-}
